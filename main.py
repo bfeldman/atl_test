02b01dde4ea0cfffcf77326b9262be8b4c89c4b3
Brian Feldman <brian.feldman42@gmail.com>
2021-01-17 17:16:36 -0500

    more

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +13,7 @@ class ArticleModel(db.Model):
   slug = db.Column(db.String, nullable=True)
   title = db.Column(db.String, nullable=True)
   dek = db.Column(db.String, nullable=True)
-  published_date = db.Column(db.DateTime, nullable=True)
+  published_date = db.Column(db.String, nullable=True)
   canonical_url = db.Column(db.String, nullable=True)
   word_count = db.Column(db.Integer, nullable=True)
   tags = db.Column(db.String, nullable=True)
@@ -27,7 +28,7 @@ article_put_args.add_argument("id", type=str, help="ID of article", required=Tru
 article_put_args.add_argument("slug", type=str, help="slug of article")
 article_put_args.add_argument("title", type=str, help="title of article")
 article_put_args.add_argument("dek", type=str, help="dek of article")
-#article_put_args.add_argument("published_date", type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H:%M:%S'), help="pub date of article")
+article_put_args.add_argument("published_date", type=str, help="pub date of article")
 article_put_args.add_argument("canonical_url", type=str, help="canonical url of article")
 article_put_args.add_argument("word_count", type=int, help="word count of article")
 article_put_args.add_argument("tags", type=str, help="tags of article")
@@ -44,19 +45,20 @@ class Article(Resource):
   
   @marshal_with(resource_fields)
   def get(self, article_id):
-    result = ArticleModel.query.filter_by(id = id).first()
+    result = ArticleModel.query.filter_by(id = article_id).first()
+    if not result:
+      abort(404, message="could not find article")
     return result
   
   @marshal_with(resource_fields)
-  def put(self, article_id):
-    print("HELLO")
+  def put(self):
     args = article_put_args.parse_args()
     article = ArticleModel(
-      id=article_id,
+      id=args['id'],
       slug=args['slug'],
       title=args['title'],
       dek=args['dek'],
-      #published_date=args['published_date'],
+      published_date=args['published_date'],
       canonical_url=args['canonical_url'],
       word_count=args['word_count'],
       tags=args['tags']
@@ -64,14 +66,9 @@ class Article(Resource):
     db.session.add(article)
     db.session.commit()
     return article, 201
-    
-  def delete(self, article_id):
-    abort_if_article_doesnt_exist(article_id)
-    del articles[article_id]
-    return '', 204
 
 
-api.add_resource(Article, "/article/<string:article_id>")
+api.add_resource(Article, "/article", "/article/<string:article_id>")
 
 if __name__ == "__main__":
   app.run(debug=True)
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ article = {
     "slug": "tracee-ellis-ross-on-hollywood-girlfriends-netflix",
     "title": "Tracee Ellis Ross Felt Lost in Hollywood. Then She Changed Course.",
     "dek": "The <em>Black-ish </em>star and Emmy nominee figured she’d made it when she scored the lead role in the early-2000s sitcom <em>Girlfriends</em>. But it would be 14 more years before she hit her stride.",
-    #"published_date": "2020-08-30T10:00:00Z",
+    "published_date": "2020-08-30T10:00:00Z",
     "canonical_url": "/culture/archive/2020/08/tracee-ellis-ross-on-hollywood-girlfriends-netflix/615754/",
     "word_count": 2068,
     "tags": "",
@@ -15,8 +15,8 @@ article = {
 
 
 #print(article['id'])
-print(BASE_URL + "article/" + article['id'])
-response = requests.put(BASE_URL + "article/" + article['id'], article)
+print(BASE_URL + "article")
+response = requests.put(BASE_URL + "article", article)
 print(response)
 
 # input()
