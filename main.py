68ed04bda264be72c7eb21807338fce07fac58db
Brian Feldman <brian.feldman42@gmail.com>
2021-01-17 20:38:17 -0500

    relationships

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-import datetime
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+#JOIN TABLES
+embed_table = db.Table('embeds',
+  db.Column('embed_id', db.Integer, db.ForeignKey('embed_model.id'), primary_key=True),
+  db.Column('article_id', db.Integer, db.ForeignKey('article_model.id'), primary_key=True)
+)
+
+author_table = db.Table('authors',
+  db.Column('author_id', db.Integer, db.ForeignKey('author_model.id'), primary_key=True),
+  db.Column('article_id', db.Integer, db.ForeignKey('article_model.id'), primary_key=True)
+)
+
+#embed model (many-to-many)
+class EmbedModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  def __repr__(self):
+    return '<Embed %r>' %self.id
+
+#author model (many-to-many)
+class AuthorModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  slug = db.Column(db.String, nullable=True)
+  
+  def __repr__(self):
+    return '<Author %r>' %self.id
+    
+# article model
 class ArticleModel(db.Model):
   id = db.Column(db.String, primary_key=True)
   slug = db.Column(db.String, nullable=True)
@@ -17,21 +42,38 @@ class ArticleModel(db.Model):
   canonical_url = db.Column(db.String, nullable=True)
   word_count = db.Column(db.Integer, nullable=True)
   tags = db.Column(db.String, nullable=True)
+  #relationships
+  lead_art = db.Column(db.Integer, db.ForeignKey('lead_art_model.id'), nullable=True)
+  embeds = db.relationship('EmbedModel', secondary=embed_table, lazy='subquery', backref=db.backref('articles', lazy=True))
+  authors = db.relationship('AuthorModel', secondary=author_table, lazy='subquery', backref=db.backref('articles', lazy=True))
   
   def __repr__(self):
     return '<Article %r>' %self.id
+    
+#lead art model (one-to-many relationship) 
+class LeadArtModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  type = db.Column(db.String, nullable=True)
+  articles = db.relationship('Article', backref=db.backref('user', lazy='joined'), lazy='dynamic'),
+  def __repr__(self):
+    return '<LeadArt %r>' %self.id
 
-# db.create_all()
+
+
+db.create_all()
 
 article_put_args = reqparse.RequestParser()
-article_put_args.add_argument("id", type=str, help="ID of article", required=True)
-article_put_args.add_argument("slug", type=str, help="slug of article", required=True)
-article_put_args.add_argument("title", type=str, help="title of article")
-article_put_args.add_argument("dek", type=str, help="dek of article")
-article_put_args.add_argument("published_date", type=str, help="pub date of article")
-article_put_args.add_argument("canonical_url", type=str, help="canonical url of article")
-article_put_args.add_argument("word_count", type=int, help="word count of article")
-article_put_args.add_argument("tags", type=str, help="tags of article")
+article_put_args.add_argument("id", type=str, required=True)
+article_put_args.add_argument("slug", type=str, required=True)
+article_put_args.add_argument("title", type=str)
+article_put_args.add_argument("dek", type=str)
+article_put_args.add_argument("published_date", type=str)
+article_put_args.add_argument("canonical_url", type=str)
+article_put_args.add_argument("word_count", type=int)
+article_put_args.add_argument("tags", type=str)
+article_put_args.add_argument("embeds", type=list)
+article_put_args.add_argument("lead_art", type=dict)
+article_put_args.add_argument("authors", type=list)
 
 
 resource_fields = { "article" : {
--- test.py
+++ test.py
@@ -3,23 +3,42 @@ import requests
 BASE_URL = "http://localhost:5000/"
 
 article = {
-    "id": "b73b8b0e-0240-42a9-874c-00445d51dd8",
+    "id": "b73b8b0e-0240-42a9-874c-00445d51dd8a",
     "slug": "tracee-ellis-ross-on-hollywood-girlfriends-netflix",
-    "title": "POOP Tracee Ellis Ross Felt Lost in Hollywood. Then She Changed Course.",
+    "title": "Tracee Ellis Ross Felt Lost in Hollywood. Then She Changed Course.",
     "dek": "The <em>Black-ish </em>star and Emmy nominee figured she’d made it when she scored the lead role in the early-2000s sitcom <em>Girlfriends</em>. But it would be 14 more years before she hit her stride.",
     "published_date": "2020-08-30T10:00:00Z",
     "canonical_url": "/culture/archive/2020/08/tracee-ellis-ross-on-hollywood-girlfriends-netflix/615754/",
     "word_count": 2068,
     "tags": "",
+    "embeds": NULL,
+    "lead_art": {
+      "id": "1526857",
+      "type": "image_large"
+    },
+    "authors":[
+      {
+        "id": "7313",
+        "slug": "shirley-li",
+      },
+      {
+        "id": "3453",
+        "slug": "lafrance-adrienne",
+      }
+    ]
   }
 
+# article2 = {
+#   "id": "TEST-123",
+#   "slug": "test-slug-9000",
+#   "title": "I'm Testing",
+#   "dek": "welcome to test land",
+#   "published_date": "2020-08-30T10:00:00Z",
+#   "canonical_url": "/url/to/thing",
+#   "word_count": 666,
+#   "tags": "test tag",
+# }
 
-#print(article['id'])
-print(BASE_URL + "article")
+
 response = requests.put(BASE_URL + "article", article)
 print(response)
-
-# input()
-# 
-# response = requests.get(BASE_URL + "article/2")
-# print(response.json())
